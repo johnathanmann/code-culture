284e65e71931d1a651978b47ff4c47af93b65645
Johnny <johnathan.mnn@gmail.com>
2023-01-31 12:04:32 -0600

    Page chages to technology page while taking in the tech type

--- src/components/Culture.js
+++ src/components/Culture.js
@@ -1,24 +1,52 @@
 import React, { useState } from 'react';
-import NavTabs from './NavTabs';
 import Footer from './Footer'
 import Home from './pages/Homepage';
 import Technology from './pages/Technology';
+import { Nav, Navbar} from 'react-bootstrap';
+import "../styles/navtabs.css";
 export default function Culture() {
   const [currentPage, setCurrentPage] = useState('Home');
+
+  const [currentTech, setCurrentTech] = useState({tech:""})
+  
+    const changeState = (techParamater) => {  
+        // handlePageChange('Technology');
+        setCurrentTech({tech:techParamater}); 
+       }; 
+
   const renderPage = () => {
     if (currentPage === 'Home') {
       return <Home handlePageChange={handlePageChange}/>;
     }
     if (currentPage === 'Technology') {
-      return <Technology handlePageChange={handlePageChange}/>;
+      return <Technology tech={currentTech.tech}/>;
     }
   };
 
   const handlePageChange = (page) => setCurrentPage(page);
 
+  const techPageChange  = (tech) => {
+    handlePageChange('Technology');
+    changeState(tech)
+  }
 
   return (
     <div>
+      <Navbar className="navbar navbar-expand-lg navbar-dark" collapseOnSelect  expand="lg">
+            <a className="navbar-brand"><h1>Code Culture</h1></a>
+          <Navbar.Toggle />
+          <Navbar.Collapse>
+            <Nav className="ms-auto d-block">
+              <ul>
+                <li>
+                <Nav.Item>
+                <a onClick={()=> techPageChange('react')}>React Page</a>
+              </Nav.Item>
+              </li>
+              </ul>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
       {renderPage()}
     </div>
   );
--- src/components/pages/Homepage.js
+++ src/components/pages/Homepage.js
@@ -11,8 +11,6 @@ export default function Homepage({handlePageChange}){
     return(
         <div>
            <h1>Homepage</h1>
-           <a onClick={() => changeState('react')}>react</a>
-           <Technology tech={currentTech.tech} /> 
         </div>
     )
 }
