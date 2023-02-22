16514ed7af8baa4932e579489fbda85da2220eaa
Johnny <johnathan.mnn@gmail.com>
2023-02-21 21:30:46 -0600

    Styled footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,11 @@
+import logo from "../assets/imgs/cc-logo.png";
+import "../styles/footer.css";
+
 export default function Footer(){
  return(
-    <div className="container">
-        
+    <div className="container-fluid text-center" id="footer">
+        <img src={logo} alt="Code culture logo which is the letter C inside a larger letter C"/>
+        <h2>Code Culture</h2>
     </div>
  )
 };
