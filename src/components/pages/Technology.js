109a98b9c0d8cbc2fbf34ffe5550d316c18914c3
Johnny <johnathan.mnn@gmail.com>
2023-03-03 19:27:16 -0600

    All icons are added on tech pages

--- src/components/pages/Technology.js
+++ src/components/pages/Technology.js
@@ -3,7 +3,15 @@ import "../../styles/technology.css";
 import greenMic from "../../assets/imgs/green-mic.png"
 import orangeMic from "../../assets/imgs/orange-mic.png"
 import yellowMic from "../../assets/imgs/yellow-mic.png"
+import greenPlay from "../../assets/imgs/green-play.png"
+import orangePlay from "../../assets/imgs/orange-play.png"
+import yellowPlay from "../../assets/imgs/yellow-play.png"
+import greenArticle from "../../assets/imgs/green-article.png"
+import orangeArticle from "../../assets/imgs/orange-article.png"
+import yellowArticle from "../../assets/imgs/yellow-article.png"
 var microphone;
+var play;
+var article;
 
 export default function Technology(props){
 
@@ -15,14 +23,20 @@ export default function Technology(props){
 
     if(wantedTech[0].type === "Language"){
         microphone = orangeMic;
+        play = orangePlay;
+        article = orangeArticle;
     }
 
     if(wantedTech[0].type === "JavaScript-Library"){
         microphone = greenMic;
+        play = greenPlay;
+        article = greenArticle;
     }
 
     if(wantedTech[0].type === "CSS-Framework"){
         microphone = yellowMic;
+        play = yellowPlay;
+        article = yellowArticle;
     }
 
       window.onscroll = function() {scrollFunction()};
@@ -52,20 +66,20 @@ export default function Technology(props){
                 <h1 className='header'>Learn More</h1>
                     <div className='col-md-4 d-flex'>
                         <div className='content-card'>
-                            <img src={microphone}/>
+                            <img alt="Microphone icon" src={microphone}/>
                             <h1>Podcast</h1>
                         </div>
                     </div>
                     <div className='col-md-4 d-flex'>
                         <div className='content-card'>
-                            <img src={microphone}/>
-                            <h1>Podcast</h1>
+                            <img alt="Play button icon" src={play}/>
+                            <h1>Video</h1>
                         </div>
                     </div>
                     <div className='col-md-4 d-flex'>
                         <div className='content-card'>
-                            <img src={microphone}/>
-                            <h1>Podcast</h1>
+                            <img alt="Article icon" src={article}/>
+                            <h1>Article</h1>
                         </div>
                     </div>
                 </div>
